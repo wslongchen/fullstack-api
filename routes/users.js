3aff77f7856e88cc62c47ab120d4423192fb418b
wslongchen <wslongchen@vip.qq.com>
2017-03-12 22:16:53 +0800

    增加mysql
    。。。

--- routes/users.js
+++ routes/users.js
@@ -3,13 +3,51 @@ var router = express.Router();
 var URL = require('url');
 
 var User = require('./user');
+// 导入MySQL模块
+var mysql = require('mysql');
+var dbConfig = require('../lib/db/mysql');
+var userSQL = require('../lib/db/Usersql');
+
+var pool = mysql.createPool( dbConfig.mysql );
+
+// 响应一个JSON数据
+var responseJSON = function (res, ret) {
+     if(typeof ret === 'undefined') { 
+          res.json({     code:'-200',     msg: '操作失败'   
+        }); 
+    } else { 
+      res.json(ret); 
+  }};
+
+  // 添加用户
+router.get('/addUser', function(req, res, next){
+ // 从连接池获取连接 
+pool.getConnection(function(err, connection) { 
+// 获取前台页面传过来的参数  
+ var param = req.query || req.params;   
+// 建立连接 增加一个用户信息 
+connection.query(userSQL.insert, [param.uid,param.name], function(err, result) {
+        if(result) {      
+             result = {   
+                      code: 200,   
+                     msg:'增加成功'
+             };  
+        }     
+
+     // 以json形式，把操作结果返回给前台页面     
+       responseJSON(res, result);   
+
+     // 释放连接  
+      connection.release();  
+
+       });
+    });
+ });
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('user api');
 });
-
-module.exports = router;
 
 router.get('/getUserInfo',function(req,res,next){
 	var user = new User();
@@ -27,3 +65,5 @@ router.get('/getUserInfo',function(req,res,next){
 	var response = {status:1,data:user};
 	res.send(JSON.stringify(response));
 });
+
+module.exports = router;
