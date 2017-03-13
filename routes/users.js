447696416c5e8b98b8054fb3b60176eff9ed47ff
潘安 <wslongchen@vip.qq.com>
2017-03-13 18:14:49 +0800

    。。。
    。。。

--- routes/users.js
+++ routes/users.js
@@ -1,69 +1,49 @@
-var express = require('express');
-var router = express.Router();
-var URL = require('url');
-
 var User = require('./user');
 // 导入MySQL模块
 var mysql = require('mysql');
-var dbConfig = require('../lib/db/mysql');
-var userSQL = require('../lib/db/Usersql');
+var dbConfig = require('../libs/db/mysql');
+var userSQL = require('../libs/db/Usersql');
+var pool = mysql.createPool(dbConfig.mysql );
+var commons = require("../libs/core/common");
+var crypto = require('crypto');
 
-var pool = mysql.createPool( dbConfig.mysql );
+// 添加用户
+exports.addUser = function(req, res, next){
+  if(!req.session.sess_admin) 
+    commons.resFail(res, 1, "需要登录才可以访问");
+  else {
+    pool.getConnection(function(err, connection) {  
+    var param = req.query || req.params;   
+    var md5 = crypto.createHash('md5');
+    var password = md5.update(param.pwd).digest('hex');
 
-// 响应一个JSON数据
-var responseJSON = function (res, ret) {
-     if(typeof ret === 'undefined') { 
-          res.json({     code:'-200',     msg: '操作失败'   
-        }); 
-    } else { 
-      res.json(ret); 
-  }};
+    connection.query(userSQL.insert, [param.uid,param.name,password,param.remark], function(err, result) {
+          if(result) {      
+            commons.resSuccess(res, "插入成功"); 
+          }else{
+            commons.resFail(res,1,"插入失败");
+          }
+        connection.release();  
+         });
+      });
+  }
+ };
 
-  // 添加用户
-router.get('/addUser', function(req, res, next){
- // 从连接池获取连接 
-pool.getConnection(function(err, connection) { 
-// 获取前台页面传过来的参数  
- var param = req.query || req.params;   
-// 建立连接 增加一个用户信息 
-connection.query(userSQL.insert, [param.uid,param.name], function(err, result) {
-        if(result) {      
-             result = {   
-                      code: 200,   
-                     msg:'增加成功'
-             };  
-        }     
-
-     // 以json形式，把操作结果返回给前台页面     
-       responseJSON(res, result);   
-
-     // 释放连接  
-      connection.release();  
-
-       });
-    });
- });
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('user api');
-});
-
-router.get('/getUserInfo',function(req,res,next){
-	var user = new User();
-	var params = URL.parse(req.url,true).query;
-	if(params.id == '1'){
-		user.name= "mrpan";
-		user.age = "23";
-		user.city = "北京市";
-	}else{
-		user.name = "longchen";
-		user.age = "22";
-		user.city = "上海市";
-	}
-
-	var response = {status:1,data:user};
-	res.send(JSON.stringify(response));
-});
-
-module.exports = router;
+// 获取用户
+exports.getUserInfo = function(req, res, next){
+  if(!req.session.sess_admin) 
+    commons.resFail(res, 1, "需要登录才可以访问");
+  else {
+    pool.getConnection(function(err, connection) {
+    var param = req.query || req.params;
+    connection.query(userSQL.getUserById, [param.uid], function(err, result) {
+          if(result) {      
+            commons.resSuccess(res, "操作成功",result); 
+          }else{
+            commons.resFail(res,1,"操作失败");
+          }
+        connection.release();  
+         });
+      });
+  }
+ };
